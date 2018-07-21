5f031c00f29e7ccef73674bd4baa87d8eb73428a
MiroMaric <miromaric1996@gmail.com>
2018-07-21 23:44:54 +0200

    Add GET /todos:id route

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,27 +1,30 @@
 const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
+
 var id = '5b538b128e906620229b8c4d';
 var userId = '5b527a5edaa97ef42a757712';
-// Todo.find({
-//     _id:id
-// }).then((docs)=>{
-//     console.log('docs: ',docs);
-// });
 
-// Todo.findOne({
-//     _id:id
-// }).then((doc)=>{
-//     console.log('doc: ',doc);
-// });
+Todo.find({
+    _id:id
+}).then((docs)=>{
+    console.log('docs: ',docs);
+});
 
-// Todo.findById(id).then((doc)=>{
-//     if(!doc){
-//         return console.log('Id not found');
-//     }
-//     console.log('Document by id: ',doc);
-// });
+Todo.findOne({
+    _id:id
+}).then((doc)=>{
+    console.log('doc: ',doc);
+});
+
+Todo.findById(id).then((doc)=>{
+    if(!doc){
+        return console.log('Id not found');
+    }
+    console.log('Document by id: ',doc);
+});
 
 //Challenge
 if(ObjectID.isValid(userId)){
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo');
@@ -27,6 +28,25 @@ app.get('/todos',(req,res)=>{
     },(err)=>{
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(400).send({});
+    }else{
+        Todo.findById(id).then((doc)=>{
+            if(!doc){
+                res.status(404).send({});
+            }else{
+                res.send(doc);
+            }
+
+        }).catch((err)=>{
+            res.status(404).send({});
+            console.log(err);
+        });
+    }
 });
 
 app.listen(3000,()=>{
